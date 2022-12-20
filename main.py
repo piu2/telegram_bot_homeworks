ea6660c82253a7027dadbc81a51160bd9b35512b
piu2 <120970353+piu2@users.noreply.github.com>
2022-12-20 18:15:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,98 @@
 
-#from datetime import date
 import telebot
-#import gspread
+import os
+import random
+import requests
 
-bot_token = 'token'
-#googlesheet_id = 'some_spreadsheet_id'
+bot_token = '5881010706:AAEd4FLdnL5VCO3Xmd_uElxn_3_7LFlUqJY'
 
 bot = telebot.TeleBot('5881010706:AAEd4FLdnL5VCO3Xmd_uElxn_3_7LFlUqJY')
-#gc = gspread.service_account()
 
-@bot.message_handler(commands=['start','help'])
+
+school, number, name = '', '', ''
+
+@bot.message_handler(commands=['start', 'help'])
 def get_text_messages(message):
     if message.text == "Привет" or message.text == "привет":
-        bot.send_message(message.from_user.id, "Привет, выбери, что нужно сделать:\nотправить домашку /send\nполучить домашку /get\nхочешь фото с котиками?) /cats")
+        bot.send_message(message.from_user.id,
+                         "Привет, выбери, что нужно сделать:\nотправить домашку /send\nполучить домашку /get\nхочешь фото с котиками?) /cats")
     elif message.text == "/start":
         bot.send_message(message.from_user.id, "Напиши привет")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /start.")
 
-@bot.message_handler(commands=['cats'])
-def cats(message):
-    bot.send_animation()
+
+
 
 @bot.message_handler(commands=['send'])
 def send_hw(message):
     bot.send_message(message.from_user.id, "Из какой ты школы?\nНапиши: 57 или Наша школа")
     bot.register_next_step_handler(message, get_school)
 
+
 def get_school(message):
     global school
     school = message.text
-    if message.text == "57":
+    if school == "57":
+        bot.send_message(message.from_user.id, "Напиши свою фамилию и имя")
         bot.register_next_step_handler(message, get_name)
-    if message.text == "Наша школа" or message.text == "Наша":
+    if school == "Наша школа" or school == "Наша":
         bot.send_message(message.from_user.id, "Из какого ты класса? Напиши: 5 или 6")
         bot.register_next_step_handler(message, get_number)
 
-#сделать тут иф
-def get_name(message):
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, "Напиши свою фамилию и имя")
-    bot.register_next_step_handler(message, get_photo)
-    #файл на диске должен называться имя и фамилия. Запомнить переменную
 
 def get_number(message):
     global number
     number = message.text
-    if message.text == "5":
+    bot.send_message(message.from_user.id, "Напиши свою фамилию и имя")
+    if number == "5":
         bot.register_next_step_handler(message, get_name)
-    if message.text == "6":
+    if number == "6":
         bot.register_next_step_handler(message, get_name)
+
+def get_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, "Пришли одно фото своей домашки или файл пдф")
+    bot.register_next_step_handler(message, get_photo)
+
 
 def get_photo(message):
-    
-
-    def save_photo_in_user_folder(message):
-        directory_for_all_users_photos = "D:\img"
-
-    user_id = message.from_user.id
-    fileID = message.photo[-1].file_id
+    try:
+        fileID = message.photo[-1].file_id
+    except TypeError:
+        fileID = message.document.file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
-
-    if not os.path.isdir(str(user_id)):
-        os.mkdir(str(user_id))
-    os.chdir(str(user_id))
-    with open("image_def_folder.jpg", 'wb') as new_file:
-        new_file.write(downloaded_file)
+    extension = file_info.file_path[-4:]
+    save_photo_in_user_folder(downloaded_file, extension)
 
 
-#def get_photo(message):
+def save_photo_in_user_folder(photo, extension):
+    global name
+    os.chdir('./домашки')
+    if not os.path.isdir(name):
+        os.mkdir(name)
+    os.chdir(name)
+    with open(str(random.randint(1e5, 1e8)).zfill(8) + extension, 'wb') as new_file:
+        new_file.write(photo)
+    os.chdir("..")
+    os.chdir('..')
 
 
-
-
-#@bot.message_handler(commands=["get"])
-
-
-#@bot.message_handler(commands=["cats"])
-#if __name__ == '__main__':
-    #executor.start_polling(dp)
+@bot.message_handler(commands=['cats'])
+def cat(message):
+    if message.text == "/cats":
+        response = requests.get("https://cataas.com/cat")
+        if response.status_code == 200:
+            with open('c.png', 'wb') as out_file:
+                out_file.write(response.content)
+            photo = open("c.png", 'rb')
+            bot.send_photo(message.from_user.id, photo, caption = 'Лови' )
+        else:
+            bot.send_message(message.from_user.id, "Технические шоколадки, извини")
 
 bot.polling(none_stop=True, interval=0)
 
-
-
-
-
-#научиться добавлять фото и отправлять их в табличку
 
 
